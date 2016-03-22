e104f28db3e70bfadc7354dab2e845055d63af29
turquoiseted <rsb524@york.ac.uk>
2016-03-22 20:08:03 +0000

    Implemented for Constants

--- Integrator.py
+++ Integrator.py
@@ -1,8 +1,25 @@
+import differentiation
+
+
 def integrate(equ):
+    """
+    Works out what kind of equation the given segment is so it can be differentiated
+    :param equ:
+    :return:
+    """
     if "x" in equ:
         return "1/2x^2"
+    else:
+        return constant_equation(equ)
+
+
+def constant_equation(funct):
+    coeff = differentiation.parse_coefficient(funct)
+    return coeff + "x"
 
 
 def tests():
     print()
-    assert "int(x)" == "1/2x^2"
+    assert "x" == "1/2x^2"
+    assert "1" == "x"
+    assert "5" == "5x"
